2de54493d2ef43157fb7e8e5d2d37142354e52a6
akikgt <akimjk1101@gmail.com>
2022-01-01 02:39:35 +0900

    First commit

---
+++ src/test/java/com/example/bank_demo/BankDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bank_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
